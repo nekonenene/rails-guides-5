0e697597b861a5a621be72f6d6558b6bf8f62c7d
Ryosuke Yokoe <yokoe_r@uuum.jp>
2016-08-26 17:45:16 +0900

    viewを用意し、newページが表示されるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  #resources: articles
-
   root 'welcome#index'
 
   get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :articles
+  # index, create, new, edit, show, update, destroy すべて作られる
+
 end
