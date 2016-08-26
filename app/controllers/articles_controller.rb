ff4b26e62c030d0f1ee2960d18bc524f2654ea9b
Ryosuke Yokoe <yokoe_r@uuum.jp>
2016-08-26 18:51:08 +0900

    add CREATE page and SHOW page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,22 @@ class ArticlesController < ApplicationController
 
   def create
     # JSON のように送信されたデータの中身を表示する
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
 
+    # "strong_parameters" の機能で拒否されないよう、セットしていいパラメーターを管理
+    # @article = Article.new(params[:article])
+    @article = Article.new(article_params)
+
+    @article.save # DB に保存
+    redirect_to @article
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
