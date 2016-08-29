f2bc033f60e0e7b58350efe5514cc139a35384f7
Ryosuke Yokoe <yokoe_r@uuum.jp>
2016-08-29 12:07:02 +0900

    編集ページとリンクの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -24,9 +28,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def index
-    @articles = Article.all
+  def edit
+    @article = Article.find(params[:id])
   end
+
 end
 
 private
